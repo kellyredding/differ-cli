ae4e4ed63473c31ca328c6640bf1e30fda7facd9
Kelly <kelly@kellyredding.com>
2012-01-25 16:39:11 -0600

    added options for methods and formatting, execute puts the diff output based on the options

--- lib/differ_cli.rb
+++ lib/differ_cli.rb
@@ -34,24 +34,69 @@ module DifferCLI
         raise Quickl::Exit, "#{Quickl.program_name} #{VERSION}"
       end
 
-      opt.on('--method METHOD', 'Specify a diff method (line, word, char)') do |method|
+      opt.on('--method METHOD', 'the diff method: line (default), word, or char') do |method|
         @differ_cli_opts[:method] = method
       end
+
+      opt.on('--format FORMAT', 'the output format: ascii (default), color, or html') do |method|
+        @differ_cli_opts[:format] = method
+      end
+
+      opt.on('-a', '--ascii', 'format output in ascii') do
+        @differ_cli_opts[:format] = 'ascii'
+      end
+
+      opt.on('-c', '--color', 'format output in color') do
+        @differ_cli_opts[:format] = 'color'
+      end
+
+      opt.on('-h', '--html', 'format output in html') do
+        @differ_cli_opts[:format] = 'html'
+      end
+
     end
 
     # Run the command
     def execute(args)
-      puts "Hello #{@differ_cli_opts[:method].inspect} #{args.join(' and ')} from #{Quickl.program_name}"
+      puts Base.new(*(args+[@differ_cli_opts || {}])).diff
     end
 
   end
 
   class Base
 
+    METHODS = ['line', 'word', 'char']
+    FORMATS = ['ascii', 'color', 'html']
+
     attr_accessor :left, :right, :diff_method, :boundary_string
 
-    def initialize(*args)
+    def initialize(*params)
+      opts, args = [
+        params.last.kind_of?(::Hash) ? params.pop : {},
+        params
+      ]
 
+      raise Quickl::Exit, "too few arguments: #{args.join(', ')}" if args.size < 2
+      raise Quickl::Exit, "too many arguments: #{args.join(', ')}" if args.size > 2
+
+      @left  = args.first
+      @right = args.last
+      @diff_method = opts[:method] || 'line'
+      @format_as = opts[:format] || 'ascii'
+    end
+
+    def diff_method=(value)
+      raise Quickl::Exit, "invalid method '#{value}'" unless METHODS.include?(value.to_s)
+      @diff_method = value
+    end
+
+    def format_as=(value)
+      raise Quickl::Exit, "invalid format '#{value}'" unless FORMATS.include?(value.to_s)
+      @format_as = value
+    end
+
+    def diff
+      ::Differ.send("diff_by_#{@diff_method}", *[@left, @right]).format_as(@format_as.to_sym)
     end
 
   end
